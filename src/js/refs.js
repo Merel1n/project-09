b0fcaa0cc367e2f0fcb3b9b893b90a6e8e5637ff
Tochka <tochka114@gmail.com>
2024-07-05 00:14:59 +0300

    update-refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,6 @@
 export const refs={
   reviewsList:document.querySelector('.reviews-list'),
   errorMassage:document.querySelector('#error-message'),
- prevButton:document.querySelector('.swiper-button-prev'),
+prevButton:document.querySelector('.swiper-button-prev'),
 nextButton:document.querySelector('.swiper-button-next'),
 }
