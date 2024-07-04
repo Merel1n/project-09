f5b5ae396469b67d2b5b01d3ca86a3743b4534cf
Tochka <tochka114@gmail.com>
2024-07-04 23:50:15 +0300

    get

---
+++ src/js/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+export async function getReviews() {
+
+  const BASE_URL = 'https://portfolio-js.b.goit.study';
+  const END_POINT = '/api/reviews';
+  const url = `${BASE_URL}${END_POINT}`;
+
+  };
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (error) {
+   alert('error')
+    throw error;
+  };
