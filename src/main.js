730065af07d1464821421b2aff5a21f378acd18e
Inna Matviienko <ineskamit@gmail.com>
2024-07-07 22:54:21 +0300

    up

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import { refs } from './js/refs';
 import './js/reviews';
 import './js/projects';
-import './js/about-me';
 import { observer } from './js/covers';
 observer.observe(refs.sectionCovers);
 import './js/faq';
 import './js/work-together';
-import './js/about-me';
-import { observer } from './js/covers';
 import { isOpenMenu } from './js/modal-menu';
 import { closeMenu } from './js/modal-menu';
 import { openMenu } from './js/menu';
@@ -15,4 +12,3 @@ observer.observe(refs.sectionCovers);
 refs.burgerMenu.addEventListener('click', isOpenMenu);
 refs.closeBurgerMenu.addEventListener('click', closeMenu);
 refs.menu.addEventListener('click', openMenu);
-
