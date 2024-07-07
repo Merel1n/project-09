f38465aa5482bfec4fe095f2430b1bf5971c702d
Inna Matviienko <ineskamit@gmail.com>
2024-07-07 18:56:53 +0300

    menu i modal-menu js

--- src/js/refs.js
+++ src/js/refs.js
@@ -4,4 +4,10 @@ export const refs = {
   nextButton: document.querySelector('.swiper-button-next'),
   prevButton: document.querySelector('.swiper-button-prev'),
   sectionCovers: document.querySelector('.section-covers'),
+  burgerMenu: document.querySelector('.burger-menu'),
+  closeBurgerMenu: document.querySelector('.close-btn'),
+  modalMenu: document.querySelector('.modal-menu'),
+  modalContainer: document.querySelector('.modal-container'),
+  menu: document.querySelector('.menu'),
+  menuList: document.querySelector('.menu-list'),
 };
