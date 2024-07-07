6b9fe3869e57f48dabbcdd3e70c7c0a0959a0d30
AnZave <crossya94@gmail.com>
2024-07-08 02:16:11 +0300

    fix-faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,20 @@
 import Accordion from 'accordion-js';
+//import sprite from './src/img/icons-sprite.svg';
 //import 'accordion-js/dist/accordion.min.css';
 
-//new Accordion('.accordion-container');
-const container = document.querySelector('.accordion-container');
+//new Accordion('.accordion-container-faq');
+const container = document.querySelector('.accordion-container-faq');
 const accordion = new Accordion(container, {
   showMultiple: true,
   onOpen: currEl => console.log('Open!', currEl),
 });
 
-const buttons = accContainer.querySelectorAll('.close-accordion-btn');
+const buttons = container.querySelectorAll('.close-accordion-btn');
 [...buttons].map((button, idx) => {
   button.addEventListener('click', () => accordion.close(idx));
 });
+
+// function accordionIcon(currEl) {
+//   if (condition) {
+//   }
+// }
