375119b099725975915622663d90ce94a56f2601
Tatyana Chuiko <chyikotatyana@gmail.com>
2024-07-07 22:01:01 +0200

    fix classes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-import './js/refs'
+import './js/refs';
 import './js/reviews';
 import './js/projects';
+import './js/about-me';
 import { observer } from './js/covers';
-observer.observe(refs.sectionCovers)
-
-
-
+observer.observe(refs.sectionCovers);
