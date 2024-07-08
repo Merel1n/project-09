b7af9e44020d51fb6e636e68dfaeb8713473ca59
Tatyana Chuiko <chyikotatyana@gmail.com>
2024-07-08 20:45:33 +0200

    update accordion about me

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,18 +1,6 @@
-import Accordion from 'accordion-js';
+import Accord from 'accordion-js';
 
-new Accordion('.accordion-container', {
+new Accord('.accord-container', {
   duration: 400,
   openOnInit: [0],
-  onOpen: function (currentElement) {
-    const down = currentElement.querySelector('.am-icon-button-down');
-    const up = currentElement.querySelector('.am-icon-button-up');
-    down.classList.toggle('open');
-    up.classList.toggle('open');
-  },
-  onClose: function (currentElement) {
-    const down = currentElement.querySelector('.am-icon-button-down');
-    const up = currentElement.querySelector('.am-icon-button-up');
-    down.classList.toggle('open');
-    up.classList.toggle('open');
-  },
 });
