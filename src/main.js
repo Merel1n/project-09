7c93111dd6a1148d3198193c0c23017d982143ef
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-07-08 10:48:39 +0200

    background-image

--- src/main.js
+++ src/main.js
@@ -7,12 +7,11 @@ observer.observe(refs.sectionCovers);
 import './js/faq';
 import './js/work-together';
 import './js/about-me';
-import { observer } from './js/covers';
+// import { observer } from './js/covers';
 import { isOpenMenu } from './js/modal-menu';
 import { closeMenu } from './js/modal-menu';
 import { openMenu } from './js/menu';
-observer.observe(refs.sectionCovers);
+// observer.observe(refs.sectionCovers);
 refs.burgerMenu.addEventListener('click', isOpenMenu);
 refs.closeBurgerMenu.addEventListener('click', closeMenu);
 refs.menu.addEventListener('click', openMenu);
-
