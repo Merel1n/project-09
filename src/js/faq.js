7ba30f795b52dddc5fab8bf6d74fe2dc455ad78c
AnZave <crossya94@gmail.com>
2024-07-08 17:54:09 +0300

    fix-icon-open

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,26 +1,19 @@
 import Accordion from 'accordion-js';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const container = document.querySelector('.accordion-container-faq');
-  const accordion = new Accordion(container, {
-    showMultiple: true,
-    activeClass: 'open',
-    onOpen: currEl => console.log('Open!', currEl),
-    onClose: currEl => console.log('Close!', currEl),
-  });
-
-  // Вибираємо всі кнопки з класом '.faq-btn'
-  const buttons = document.querySelectorAll('.faq-btn');
-  buttons.forEach(button => {
-    button.addEventListener('click', function () {
-      const iconUse = button.querySelector('svg use');
-      const currentIcon = iconUse.getAttribute('href');
-
-      if (currentIcon === './img/icons-sprite.svg#icon-down-arrow') {
-        iconUse.setAttribute('href', './img/icons-sprite.svg#icon-up-arrow');
-      } else {
-        iconUse.setAttribute('href', './img/icons-sprite.svg#icon-down-arrow');
-      }
-    });
-  });
+const container = document.querySelector('.accordion-container-faq');
+const accordion = new Accordion(container, {
+  duration: 400,
+  showMultiple: true,
+  onOpen: function (currentElement) {
+    const down = currentElement.querySelector('.an-icon-button-down');
+    const up = currentElement.querySelector('.an-icon-button-up');
+    down.classList.toggle('open');
+    up.classList.toggle('open');
+  },
+  onClose: function (currentElement) {
+    const down = currentElement.querySelector('.an-icon-button-down');
+    const up = currentElement.querySelector('.an-icon-button-up');
+    down.classList.toggle('open');
+    up.classList.toggle('open');
+  },
 });
