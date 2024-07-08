b62bf1cda81b943a0d668bb7d3b4c3a009563aa7
AnZave <crossya94@gmail.com>
2024-07-08 04:24:54 +0300

    accordionIcon

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,20 +1,28 @@
 import Accordion from 'accordion-js';
-//import sprite from './src/img/icons-sprite.svg';
-//import 'accordion-js/dist/accordion.min.css';
 
-//new Accordion('.accordion-container-faq');
-const container = document.querySelector('.accordion-container-faq');
-const accordion = new Accordion(container, {
-  showMultiple: true,
-  onOpen: currEl => console.log('Open!', currEl),
+document.addEventListener('DOMContentLoaded', function () {
+  const container = document.querySelector('.accordion-container-faq');
+  const accordion = new Accordion(container, {
+    showMultiple: true,
+    activeClass: 'open',
+    onOpen: currEl => console.log('Open!', currEl),
+    onClose: currEl => console.log('Close!', currEl),
+  });
+
+  // Вибираємо всі кнопки з класом '.faq-btn'
+  const buttons = document.querySelectorAll('.faq-btn');
+  buttons.forEach(button => {
+    button.addEventListener('click', function () {
+      const iconUse = button.querySelector('svg use');
+      const currentIcon = iconUse.getAttribute('href');
+
+      if (currentIcon === '/img/icons-sprite.svg#icon-down-arrow') {
+        console.log('UP');
+        iconUse.setAttribute('href', '/img/icons-sprite.svg#icon-up-arrow');
+      } else {
+        console.log('DOWN');
+        iconUse.setAttribute('href', '/img/icons-sprite.svg#icon-down-arrow');
+      }
+    });
+  });
 });
-
-const buttons = container.querySelectorAll('.close-accordion-btn');
-[...buttons].map((button, idx) => {
-  button.addEventListener('click', () => accordion.close(idx));
-});
-
-// function accordionIcon(currEl) {
-//   if (condition) {
-//   }
-// }
