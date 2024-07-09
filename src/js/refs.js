7d2542e6f6d17ebd0e5cae328b3fad4a0e02f10f
Inna Matviienko <ineskamit@gmail.com>
2024-07-09 20:52:23 +0300

    update-reviews-js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,6 @@
 export const refs = {
   reviewsList: document.querySelector('.reviews-wrapper'),
   errorMessage: document.getElementById('error-message'),
-  nextButton: document.querySelector('.swiper-button-next'),
-  prevButton: document.querySelector('.swiper-button-prev'),
   sectionCovers: document.querySelector('.section-covers'),
   burgerMenu: document.querySelector('.burger-menu'),
   closeBurgerMenu: document.querySelector('.close-btn'),
--- src/js/reviews.js
+++ src/js/reviews.js
@@ -47,26 +47,13 @@ export async function renderReviews() {
         1440: {
           slidesPerView: 4,
           spaceBetween: 16,
-
-        }
+        },
       },
 
       keyboard: {
         enabled: true,
         onlyInViewport: false,
       },
-      on: {
-        reachEnd: function() {
-          refs.nextButton.classList.add('swiper-button-disabled');
-        },
-        reachBeginning: function() {
-          refs.prevButton.classList.add('swiper-button-disabled');
-        },
-        fromEdge: function() {
-          refs.nextButton.classList.remove('swiper-button-disabled');
-          refs.prevButton.classList.remove('swiper-button-disabled');
-        }
-      }
     });
   } catch (error) {
     refs.errorMessage.textContent = 'Not found';
